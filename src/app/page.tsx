acd4325dd23c273832b254273a11ddae0ea0594f
suprabho <suprabho13@gmail.com>
2025-06-17 09:29:09 +0530

    updated dependecies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,13 @@
 
 import { C1Chat } from "@thesysai/genui-sdk";
 import "@crayonai/react-ui/styles/index.css";
+import { themePresets } from "@crayonai/react-ui";
+import "../styles/custom.css";
 
 export default function Home() {
-  return <C1Chat apiUrl="/api/chat" theme={{ mode: "dark" }} />;
+  return <C1Chat 
+  apiUrl="/api/chat" 
+  agentName="Promad"
+  logoUrl="https://www.promad.ai/logo.png"
+  theme={{...themePresets.neon, mode: "dark"}} />;
 }
